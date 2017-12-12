bbcb1462177a8bfa97c3442ac6fd462784746e45
kento murai <kent0319.purple@gmail.com>
2017-12-12 13:03:40 +0000

    scaffold生成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210030437) do
+ActiveRecord::Schema.define(version: 20171212105032) do
+
+  create_table "business_alerts", force: true do |t|
+    t.date     "date"
+    t.string   "title"
+    t.text     "detail"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "businesses", force: true do |t|
     t.string   "alerts"
